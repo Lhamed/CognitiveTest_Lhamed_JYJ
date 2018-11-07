cc4e7fb14dade43a9af2e8e434016b1ce5f61b7b
Unknown <dbswo320@catholic.ac.kr>
2018-11-08 02:57:04 +0900

    실험은 일단 끝났다 . - Catholic Univ.정윤재

---
+++ Cognitive_Test_CueingEffect/Assets/Parser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using JsonFx.Json;
+public class Parser : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		TextAsset[] assets= Resources.LoadAll<TextAsset>("Data");
+		string jsonTemp = "";
+		List<DataTested> testListTemp = new List<DataTested>();
+
+		foreach(var asset in assets)
+		{
+			DataTested temp = 	JsonReader.Deserialize<DataTested>(asset.ToString());
+			testListTemp.Add(temp);
+		
+		}
+		jsonTemp = JsonWriter.Serialize(testListTemp.ToArray());
+		string path = Application.dataPath + "/Resources/Data/TestDataTotal"  + ".json";
+		System.IO.File.WriteAllText(path, jsonTemp);
+		AssetDatabase.ImportAsset("/Resources/Data/TestDataTotal" + ".json");
+		Debug.Log("Saved"); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
